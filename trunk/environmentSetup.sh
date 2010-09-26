dad0d68d867518b3b0c5eb965058d98c3d9a0983
marius.andreiana@gmail.com <marius.andreiana@gmail.com@0b4cfbff-20b9-c936-ea97-f0e3fcb249b9>
2010-09-26 19:09:32 +0000

    added

---
+++ trunk/environmentSetup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+yum -y install java-1.6.0-openjdk-devel eclipse-anyedit eclipse-checkstyle eclipse-dtp eclipse-emf eclipse-jdt eclipse-mylyn eclipse-mylyn-java eclipse-platform eclipse-pydev eclipse-pydev-mylyn eclipse-subclipse eclipse-svnkit git maven2-plugin-eclipse mercurial
+
