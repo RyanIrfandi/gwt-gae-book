141dadf44106100ce09bb29c276860b28bee8ff5
marius.andreiana@gmail.com <marius.andreiana@gmail.com@0b4cfbff-20b9-c936-ea97-f0e3fcb249b9>
2010-10-19 11:30:23 +0000

    gwt 2.1rc1 update

--- trunk/CultureShows/src/org/gwtgaebook/CultureShows/server/DispatchServletModule.java
+++ trunk/CultureShows/src/org/gwtgaebook/CultureShows/server/DispatchServletModule.java
@@ -10,9 +10,7 @@ public class DispatchServletModule extends ServletModule {
 	public void configureServlets() {
 		// filter("*").through(HttpSessionSecurityCookieFilter.class);
 		// bindConstant().annotatedWith(SecurityCookie.class).to("MYAPPAUTH");
-		// TODO If you use GWT 2.1M3, they changed a little bit the path of RPC
-		// request, now myApp must be removed from the serve path
-		serve("/main/" + ActionImpl.DEFAULT_SERVICE_NAME + "*").with(
+		serve("/" + ActionImpl.DEFAULT_SERVICE_NAME + "*").with(
 				DispatchServiceImpl.class);
 	}
 
