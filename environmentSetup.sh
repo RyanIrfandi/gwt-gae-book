e82f9865f0fa399550227e376bdbe1d801367eb4
marius.andreiana <marius.andreiana@gmail.com>
2010-10-21 19:07:50 +0000

    marked script as optional

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-yum -y install java-1.6.0-openjdk-devel eclipse-anyedit eclipse-checkstyle eclipse-dtp eclipse-emf eclipse-jdt eclipse-mylyn eclipse-mylyn-java eclipse-platform eclipse-pydev eclipse-pydev-mylyn eclipse-subclipse eclipse-svnkit git maven2-plugin-eclipse mercurial
+#optional bash script to setup your development environment on Fedora Linux OS
+yum -y install 'java-*-openjdk-devel' eclipse-anyedit eclipse-checkstyle eclipse-dtp eclipse-emf eclipse-jdt eclipse-mylyn eclipse-mylyn-java eclipse-platform eclipse-pydev eclipse-pydev-mylyn eclipse-subclipse eclipse-svnkit git maven-eclipse-plugin mercurial
 
