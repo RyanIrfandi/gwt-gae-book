25d7705e527815dda3a173920b4efea7c6488133
marius.andreiana@gmail.com <marius.andreiana@gmail.com@0b4cfbff-20b9-c936-ea97-f0e3fcb249b9>
2010-10-21 18:48:57 +0000

    nav

--- wiki/assets/buildTOC.php
+++ wiki/assets/buildTOC.php
@@ -21,16 +21,34 @@ $docs = array(
 	"",
 */
 
+define("NAV_START", "<wiki:comment>NAV_START</wiki:comment>");
+define("NAV_END", "<wiki:comment>NAV_END</wiki:comment>");
+define("NAV_ROOT", "http://code.google.com/p/gwt-gae-book/wiki/"); 
+//workaround for http://code.google.com/p/support/issues/detail?id=4563
+
+class Chapter 
+{
+	public $href;
+	public $name;
+};
+
+$chapters = array(); 
+
 $tocShort = "[TableOfContents Table of contents]\n\n";
 $tocDetail = "= Table of contents =\n\n";
 foreach ($docs as $doc) {
-	$f = fopen($doc . ".wiki", "ro");
+	$f = fopen($doc . ".wiki", "r");
 	$s = fread($f, 1000000);
 	fclose($f);
 
 	preg_match("/(?<=^#summary\s)[^\n]*/m", $s, $matches);
 	//print_r($matches);
 	$summary = trim($matches[0]);
+
+	$c = new Chapter();
+	$c->href = $doc;
+	$c->name = $summary;
+	$chapters[] = $c;
 	
 	$tocShort .= "[$doc $summary]\n\n";
 
@@ -61,6 +79,42 @@ fclose($f);
 $f = fopen("TableOfContents.wiki", "w");
 fwrite($f, $tocDetail);
 fclose($f);
+
+//print_r($chapters);
+// generate page navigation
+for ($i = 0; $i<count($chapters); $i++) {
+	echo "\nProcessing " . $chapters[$i]->href . " " . $chapters[$i]->name;
+	$f = fopen($chapters[$i]->href . ".wiki", "r");
+	$s = fread($f, 1000000);
+	fclose($f);
+
+	$s = str_replace('<img src="http://gwt-gae-book.googlecode.com/svn/wiki/assets/envelope.png" border="0"  /><a href="http://code.google.com/p/gwt-gae-book/issues/entry">Send feedback</a>', "", $s);
+	$s = preg_replace("/" . str_replace("/", "\/", NAV_START) . "(.*)" . str_replace("/", "\/", NAV_END) . "/ms", "", $s);
+	
+	$s .= NAV_START . "\n";
+	$s .= "<a href='http://code.google.com/p/gwt-gae-book/issues/entry'><img src='http://gwt-gae-book.googlecode.com/svn/wiki/assets/envelope.png' border='0' alt='Send feedback' /></a>" . "\n";
+	$s .= "<img src='http://gwt-gae-book.googlecode.com/svn/wiki/assets/spacer.png' border='0' alt='' />" . "\n";
+
+	if ($i>0) {
+		$s .= "<a href='" . NAV_ROOT . $chapters[$i-1]->href . "'><img src='http://gwt-gae-book.googlecode.com/svn/wiki/assets/prev.png' border='0' alt='Previous chapter: " . $chapters[$i-1]->name . "' /></a>" . "\n";
+	}
+
+	$s .= "<a href='" . NAV_ROOT . "TableOfContents'><img src='http://gwt-gae-book.googlecode.com/svn/wiki/assets/contents.png' border='0' alt='Table Of Contents' /></a>" . "\n";
+
+	if ($i<(count($chapters)-1)) {
+		$s .= "<a href='" . NAV_ROOT . $chapters[$i+1]->href . "'><img src='http://gwt-gae-book.googlecode.com/svn/wiki/assets/next.png' border='0' alt='Next chapter: " . $chapters[$i+1]->name . "' /></a>" . "\n";
+	}
+
+	$s .= NAV_END;
+
+	//echo $s;
+	$f = fopen($chapters[$i]->href . ".wiki", "w");
+	fwrite($f, $s);
+	fclose($f);
+
+	if ($i == 3) exit();
+}
+
 
 
 ?>
