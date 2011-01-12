a8b38f1ccabe8baeb1d3d721d468d433d8eebc91
marius.andreiana <marius.andreiana@gmail.com>
2011-01-12 20:17:11 +0000

    cleanup

--- setupTemplate.sh
+++ setupTemplate.sh
@@ -1,6 +1,6 @@
-rm -rf template/.apt_generated/
-rm -rf template/war/WEB-INF/classes/
-tar cvfz template.tgz --exclude=".svn" template 
-
-
-
+mkdir tmp
+cd tmp
+svn checkout http://gwt-gae-book.googlecode.com/svn/trunk/template/ template
+tar cvfz ../template-1.0.tgz --exclude=".svn" template
+cd ..
+rm -rf tmp
